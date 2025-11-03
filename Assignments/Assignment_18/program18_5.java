ee27ff5587d50ec6b60334aaeac765a4d51a8f1e
tejas-ahirrao <tejasahirrao1@gmail.com>
2025-11-03 22:52:41 +0530

    Update program18_5.java

--- Assignments/Assignment_18/program18_5.java
+++ Assignments/Assignment_18/program18_5.java
@@ -7,7 +7,7 @@ class Logic
     //  Description   : To check the number is negative, positive or zero
     //  Input         : int
     //  Output        : void
-    //  Author        : Prajwal Sanjay Pansare
+    //  Author        : Tejas Sunil Ahirrao
     //  Date          : 27/10/2025
     //
     //////////////////////////////////////////////////////////////
