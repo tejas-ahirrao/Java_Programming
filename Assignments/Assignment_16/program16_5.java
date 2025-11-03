ba2598d00ec305a1bc2cd18f61eb9dca36e7e9a1
tejas-ahirrao <tejasahirrao1@gmail.com>
2025-11-03 22:51:01 +0530

    Update program16_5.java

--- Assignments/Assignment_16/program16_5.java
+++ Assignments/Assignment_16/program16_5.java
@@ -7,7 +7,7 @@ class Logic
     //  Description   : To counting the digits of the numbers
     //  Input         : int
     //  Output        : void
-    //  Author        : Prajwal Sanjay Pansare
+    //  Author        : Tejas Sunil Ahirrao
     //  Date          : 27/10/2025
     //
     //////////////////////////////////////////////////////////////
