4c48b7a189b87b8b15ef5ef9a625fe8aa00c02f1
tejas-ahirrao <tejasahirrao1@gmail.com>
2025-11-03 22:50:34 +0530

    Update program16_2.java

--- Assignments/Assignment_16/program16_2.java
+++ Assignments/Assignment_16/program16_2.java
@@ -7,7 +7,7 @@ class Logic
     //  Description   : To check whether a given number is even or odd
     //  Input         : int
     //  Output        : void
-    //  Author        : Prajwal Sanjay Pansare
+    //  Author        : Tejas Sunil Ahirrao
     //  Date          : 27/10/2025
     //
     //////////////////////////////////////////////////////////////
