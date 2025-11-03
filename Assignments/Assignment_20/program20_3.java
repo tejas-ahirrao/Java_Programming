87995d11b856c549c2ee2513876ce50397cbc9a5
tejas-ahirrao <tejasahirrao1@gmail.com>
2025-11-03 22:54:03 +0530

    Update program20_3.java

--- Assignments/Assignment_20/program20_3.java
+++ Assignments/Assignment_20/program20_3.java
@@ -6,7 +6,7 @@ class Logic
     //  Description   : To check number is perfect or not
     //  Input         : int
     //  Output        : void
-    //  Author        : Prajwal Sanjay Pansare
+    //  Author        : Tejas Sunil Ahirrao
     //  Date          : 27/10/2025
     //
     //////////////////////////////////////////////////////////////
