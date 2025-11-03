2290e29d44e78c66f2e1c87dfcba2814583644c9
tejas-ahirrao <tejasahirrao1@gmail.com>
2025-11-03 22:52:02 +0530

    Update program18_1.java

--- Assignments/Assignment_18/program18_1.java
+++ Assignments/Assignment_18/program18_1.java
@@ -7,7 +7,7 @@ class Logic
     //  Description   : To check the number is prime or not
     //  Input         : int
     //  Output        : void
-    //  Author        : Prajwal Sanjay Pansare
+    //  Author        : Tejas Sunil Ahirrao
     //  Date          : 27/10/2025
     //
     //////////////////////////////////////////////////////////////
