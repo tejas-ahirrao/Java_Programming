920f4058e7ef3935d7c9a55521cb6cc1a6370173
tejas-ahirrao <tejasahirrao1@gmail.com>
2025-11-03 22:54:19 +0530

    Update program20_5.java

--- Assignments/Assignment_20/program20_5.java
+++ Assignments/Assignment_20/program20_5.java
@@ -6,7 +6,7 @@ class Logic
     //  Description   : To get largest digit from number
     //  Input         : int
     //  Output        : void
-    //  Author        : Prajwal Sanjay Pansare
+    //  Author        : Tejas Sunil Ahirrao
     //  Date          : 27/10/2025
     //
     //////////////////////////////////////////////////////////////
