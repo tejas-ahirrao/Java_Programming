15d7a3a5dab70248f69eba66e17c2ab5887a0fb2
tejas-ahirrao <tejasahirrao1@gmail.com>
2025-11-03 22:55:07 +0530

    Update program21_4.java

--- Assignments/Assignment_21/program21_4.java
+++ Assignments/Assignment_21/program21_4.java
@@ -6,7 +6,7 @@ class Logic
     //  Description   : To count total number of factors
     //  Input         : int
     //  Output        : void
-    //  Author        : Prajwal Sanjay Pansare
+    //  Author        : Tejas Sunil Ahirrao
     //  Date          : 27/10/2025
     //
     //////////////////////////////////////////////////////////////
