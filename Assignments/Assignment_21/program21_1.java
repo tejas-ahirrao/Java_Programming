fbf01de5da742d048ae9358aee0d0dc118ab93cf
tejas-ahirrao <tejasahirrao1@gmail.com>
2025-11-03 22:54:31 +0530

    Update program21_1.java

--- Assignments/Assignment_21/program21_1.java
+++ Assignments/Assignment_21/program21_1.java
@@ -6,7 +6,7 @@ class Logic
     //  Description   : To get multiplication of digits
     //  Input         : int
     //  Output        : void
-    //  Author        : Prajwal Sanjay Pansare
+    //  Author        : Tejas Sunil Ahirrao
     //  Date          : 27/10/2025
     //
     //////////////////////////////////////////////////////////////
