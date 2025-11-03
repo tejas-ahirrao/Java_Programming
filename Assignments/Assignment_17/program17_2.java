6641b443e642b4cc6454b697e4b39f85b5b6e8d0
tejas-ahirrao <tejasahirrao1@gmail.com>
2025-11-03 22:51:22 +0530

    Update program17_2.java

--- Assignments/Assignment_17/program17_2.java
+++ Assignments/Assignment_17/program17_2.java
@@ -7,7 +7,7 @@ class Logic
     //  Description   : To get check the number is palindrome or not
     //  Input         : int
     //  Output        : void
-    //  Author        : Prajwal Sanjay Pansare
+    //  Author        : Tejas Sunil Ahirrao
     //  Date          : 27/10/2025
     //
     //////////////////////////////////////////////////////////////
