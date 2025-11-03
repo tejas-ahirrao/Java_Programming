d698196d23ab9571e4cc555b4ed48691559c25ff
tejas-ahirrao <tejasahirrao1@gmail.com>
2025-11-03 22:53:00 +0530

    Update program19_2.java

--- Assignments/Assignment_19/program19_2.java
+++ Assignments/Assignment_19/program19_2.java
@@ -6,7 +6,7 @@ class Logic
     //  Description   : To display grade by marks
     //  Input         : int
     //  Output        : void
-    //  Author        : Prajwal Sanjay Pansare
+    //  Author        : Tejas Sunil Ahirrao
     //  Date          : 27/10/2025
     //
     //////////////////////////////////////////////////////////////
