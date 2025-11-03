7b2617f49dcc935e3e2938c2d53d49e0875c2e04
tejas-ahirrao <tejasahirrao1@gmail.com>
2025-11-03 22:52:31 +0530

    Update program18_4.java

--- Assignments/Assignment_18/program18_4.java
+++ Assignments/Assignment_18/program18_4.java
@@ -7,7 +7,7 @@ class Logic
     //  Description   : To print sum of even and odd separetely
     //  Input         : int
     //  Output        : void
-    //  Author        : Prajwal Sanjay Pansare
+    //  Author        : Tejas Sunil Ahirrao
     //  Date          : 27/10/2025
     //
     //////////////////////////////////////////////////////////////
