1c0c5ecbe2badf441d2dd34774bce99bfe391f30
tejas-ahirrao <tejasahirrao1@gmail.com>
2025-11-03 22:52:11 +0530

    Update program18_2.java

--- Assignments/Assignment_18/program18_2.java
+++ Assignments/Assignment_18/program18_2.java
@@ -7,7 +7,7 @@ class Logic
     //  Description   : To print Even numbres up to N
     //  Input         : int
     //  Output        : void
-    //  Author        : Prajwal Sanjay Pansare
+    //  Author        : Tejas Sunil Ahirrao
     //  Date          : 27/10/2025
     //
     //////////////////////////////////////////////////////////////
