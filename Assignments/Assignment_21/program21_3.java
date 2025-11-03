41a16a570ce1cec19217728f4b30ddcb2172848b
tejas-ahirrao <tejasahirrao1@gmail.com>
2025-11-03 22:54:57 +0530

    Update program21_3.java

--- Assignments/Assignment_21/program21_3.java
+++ Assignments/Assignment_21/program21_3.java
@@ -6,7 +6,7 @@ class Logic
     //  Description   : To diplay all factors of number
     //  Input         : int
     //  Output        : void
-    //  Author        : Prajwal Sanjay Pansare
+    //  Author        : Tejas Sunil Ahirrao
     //  Date          : 27/10/2025
     //
     //////////////////////////////////////////////////////////////
