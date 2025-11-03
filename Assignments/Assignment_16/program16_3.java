d7f2213949152affa75d3105c9acab87353de997
tejas-ahirrao <tejasahirrao1@gmail.com>
2025-11-03 22:50:43 +0530

    Update program16_3.java

--- Assignments/Assignment_16/program16_3.java
+++ Assignments/Assignment_16/program16_3.java
@@ -7,7 +7,7 @@ class Logic
     //  Description   : To get factorial of input
     //  Input         : int
     //  Output        : void
-    //  Author        : Prajwal Sanjay Pansare
+    //  Author        : Tejas Sunil Ahirrao
     //  Date          : 27/10/2025
     //
     //////////////////////////////////////////////////////////////
