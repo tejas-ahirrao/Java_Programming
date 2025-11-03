04f37ad3c4a119f6348c39e63328bc84b90c8eaf
tejas-ahirrao <tejasahirrao1@gmail.com>
2025-11-03 22:51:14 +0530

    Update program17_1.java

--- Assignments/Assignment_17/program17_1.java
+++ Assignments/Assignment_17/program17_1.java
@@ -7,7 +7,7 @@ class Logic
     //  Description   : To get Additon of the digits
     //  Input         : int
     //  Output        : void
-    //  Author        : Prajwal Sanjay Pansare
+    //  Author        : Tejas Sunil Ahirrao
     //  Date          : 27/10/2025
     //
     //////////////////////////////////////////////////////////////
