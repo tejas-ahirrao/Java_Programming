45bed72df0d79da329a5044c533079a8b3cc3464
tejas-ahirrao <tejasahirrao1@gmail.com>
2025-11-03 22:53:19 +0530

    Update program19_4.java

--- Assignments/Assignment_19/program19_4.java
+++ Assignments/Assignment_19/program19_4.java
@@ -6,7 +6,7 @@ class Logic
     //  Description   : To Display the digits of the numbers
     //  Input         : int
     //  Output        : void
-    //  Author        : Prajwal Sanjay Pansare
+    //  Author        : Tejas Sunil Ahirrao
     //  Date          : 27/10/2025
     //
     //////////////////////////////////////////////////////////////
